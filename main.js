fecf56902a30da451adcb949c805a4952db3378d
Rivifriedman <rivifriedman@gmail.com>
2023-01-02 21:49:38 -0500

    working countdown

---
+++ main.js
@@ -0,0 +1,38 @@
+(function () {
+    'use strict';
+
+    function get(id) {
+        return document.getElementById(id);
+    }
+
+    function countdown() {
+
+        let date = new Date();
+
+        const hoursDiv = get("hours");
+        let hoursLeft = 23 - date.getHours();
+        if (hoursLeft < 10) {
+            hoursLeft = "0" + hoursLeft;
+        }
+        hoursDiv.innerText = hoursLeft;
+
+        const minutesDiv = get("minutes");
+        let minutesLeft = 60 - date.getMinutes();
+        if (minutesLeft < 10) {
+            minutesLeft = "0" + minutesLeft;
+        }
+        minutesDiv.innerText = minutesLeft;
+
+        const secondsDiv = get("seconds");
+        let secondsLeft = 60 - date.getSeconds();
+        if (secondsLeft < 10) {
+            secondsLeft = "0" + secondsLeft;
+        }
+        secondsDiv.innerText = secondsLeft;
+    }
+
+    const timer = setInterval(function () {
+        countdown();
+    }, 1000);
+
+})();
