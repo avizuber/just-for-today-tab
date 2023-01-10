7a19c26c49e81b4c8aa268bcde37dc3686ade111
rivkafriedman <rivkafriedman10@gmail.com>
2023-01-10 18:25:09 -0500

    fixed: clearer sunset image

--- main2.js
+++ main2.js
@@ -40,7 +40,7 @@
     }
 
     const backgrounds = [
-        'url(images2/rocio-guillen-bmVs4mDwI3k-unsplash-edited.jpg)',
+        'url(images2/jason-blackeye-GPPAjJicemU-unsplash.jpg)',
         'url(images2/cristian-giordano-qtqcSwzekFo-unsplash.jpg)',
         'url(images2/lorenzo-lamonica-PJAXlV_H8-4-unsplash.jpg)'
     ];
