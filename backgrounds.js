1c463c745faeec73d5de4de3d3405d39c8f59b5e
Avi Zuber <avi.zuber@gmail.com>
2023-02-21 21:08:12 +0200

    code cleanups, gave up on ES6, switched to flexbox for responsiveness, tweaked design per feedback

--- backgrounds.js
+++
@@ -1,5 +0,0 @@
-export const backgrounds = [
-    'url(images2/jason-blackeye-GPPAjJicemU-unsplash.jpg)',
-    'url(images2/cristian-giordano-qtqcSwzekFo-unsplash.jpg)',
-    'url(images2/lorenzo-lamonica-PJAXlV_H8-4-unsplash.jpg)'
-];
